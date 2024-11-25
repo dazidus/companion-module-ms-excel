9715b477487bc4115e9d5ce5b039fba18885b62d
dazidus <128101957+dazidus@users.noreply.github.com>
2024-11-25 17:41:17 +0100

    Update variables.js

--- variables.js
+++ variables.js
@@ -1,7 +1,30 @@
 module.exports = function (self) {
 	self.setVariableDefinitions([
-		{ variableId: 'variable1', name: 'My first variable' },
-		{ variableId: 'variable2', name: 'My second variable' },
-		{ variableId: 'variable3', name: 'Another variable' },
-	])
+		{
+			variableId: 'cell_value_A1',
+			name: 'Value of Cell A1',
+		},
+		{
+			variableId: 'cell_value_B1',
+			name: 'Value of Cell B1',
+		},
+		{
+			variableId: 'default_sheet_name',
+			name: 'Default Sheet Name',
+		},
+	]);
+};
+async function updateVariables() {
+	try {
+		const valueA1 = await self.getCellValue('A1');
+		const valueB1 = await self.getCellValue('B1');
+
+		self.setVariableValues({
+			cell_value_A1: valueA1,
+			cell_value_B1: valueB1,
+			default_sheet_name: self.config.sheetName,
+		});
+	} catch (error) {
+		self.log('error', `Failed to update variables: ${error.message}`);
+	}
 }
